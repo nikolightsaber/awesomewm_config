336d90df2721aaa2eaac0b97d47e266d65c6bf0b
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-07-16 18:29:03 +0200

    make alt key + theme powerarrow-dark

--- rc.lua
+++ rc.lua
@@ -49,7 +49,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/powerarrow-dark")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
@@ -61,7 +61,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+modkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
