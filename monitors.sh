f2fd892bbe64e7c996b42668833a1c6060e29f0c
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2022-07-25 17:10:16 +0200

    fix monitors

--- monitors.sh
+++ monitors.sh
@@ -1,3 +1,4 @@
 xrandr --output eDP1 --auto --pos 1344x1296 --output DP1-1 --auto --pos 0x0 --scale 1.2x1.2 --output DP1-2 --auto --pos 2304x0 --scale 1.2x1.2
 xrandr --output eDP-1 --auto --output DP-1 --auto --above eDP-1
-xrandr --output eDP-1 --auto --pos 960x1080 --output DP-1-1 --auto --pos 0x0 --output DP-1-2 --auto --pos 1920x
+xrandr --output eDP-1 --auto --pos 960x1080 --output DP-1-1 --auto --pos 0x0 --output DP-1-2 --auto --pos 1920x0
+xrandr --output eDP-1 --auto --output HDMI-2 --auto --above eDP-1
